4dd6805d927309bc868ff9ffb66e6e2667b3e6e0
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-11 00:59:07 +0530

    feat: add repository method to fetch expense by id

--- src/repositories/expense.repository.js
+++ src/repositories/expense.repository.js
@@ -1,6 +1,11 @@
+import { eq } from "drizzle-orm";
 import db from "../db/index.js";
 import { expenses } from "../models/schema.js";
 
 export const create = async (data) => {
     return await db.insert(expenses).values(data);
 };
+
+export const findById = async (id) => {
+    return await db.select().from(expenses).where(eq(expenses.id, id));
+}
