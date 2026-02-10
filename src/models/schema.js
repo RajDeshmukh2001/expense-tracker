92dd4aff4f959916decc16278be0c890a06ec489
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-10 16:44:17 +0530

    feat(schema): add users with with id, name, email, and createdAt fields

---
+++ src/models/schema.js
@@ -0,0 +1,8 @@
+import { bigint, decimal, mysqlEnum, mysqlTable, serial, timestamp, varchar } from "drizzle-orm/mysql-core";
+
+export const users = mysqlTable("users", {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 100 }).notNull(),
+    email: varchar("email", { length: 150 }).notNull().unique(),
+    createdAt: timestamp("created_at").defaultNow(),
+});
