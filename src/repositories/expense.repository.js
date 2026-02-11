062158330dbe9674adb4e3b94ebe1b192e5e9e0a
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-11 15:52:48 +0530

    fix: src/repositories/expense.repository.js

--- src/repositories/expense.repository.js
+++ src/repositories/expense.repository.js
@@ -10,6 +10,6 @@ export const findById = async (id, userId) => {
     return await db.select().from(expenses).where(and(eq(expenses.id, id), eq(expenses.userId, userId)));
 }
 
-export const update = async (id, data) => {
-    return await db.update(expenses).set(data).where(eq(expenses.id, id));
+export const update = async (id, userId, data) => {
+    return await db.update(expenses).set(data).where(and(eq(expenses.id, id), eq(expenses.userId, userId)));
 }
