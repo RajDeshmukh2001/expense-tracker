332787023c7357bb09bae4a31efbc791dfacc3f9
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-11 14:53:36 +0530

    fix: handle max amount error with appropriate message

--- src/validations/expense.validation.js
+++ src/validations/expense.validation.js
@@ -19,7 +19,7 @@ const isWithinYear = (date) => {
 export const createExpenseSchema = z.object({
     title: z.string("Title must be a string").trim().min(1, "Title is required").max(100, "Title cannot exceed 100 characters"),
     description: z.string("Description must be a string").min(1, "Description is required").max(1000, "Description cannot exceed 1000 characters"),
-    amount: z.number("Amount must be a number").min(1, "Amount is required").positive("Amount must be greater than zero"),
+    amount: z.number("Amount must be a number").min(1, "Amount is required").max(999,999,999,999,999.99, "Amount is too large").positive("Amount must be greater than zero"),
     category: z.string("Category must be a string").trim().min(1, "Category is required").toUpperCase(),
     paymentMethod: z.refine((val) => paymentMethod.includes(val.toUpperCase()), { message: `Invalid payment method. Must be ${paymentMethod.join(", ")}` }),
     transactionDate: z.coerce.date("Transaction date must be a valid date").refine(isWithinYear, "Transaction date must be within the current financial year"),
