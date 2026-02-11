3d5e52045af35e547a4e43a5600b5273e9aa7f38
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-11 14:45:49 +0530

    fix: update transaction date validation to use financial year (April to March)

--- src/validations/expense.validation.js
+++ src/validations/expense.validation.js
@@ -4,11 +4,16 @@ const paymentMethod = ["CASH", "DEBIT_CARD", "UPI", "CREDIT_CARD"];
 
 const isWithinYear = (date) => {
     const now = new Date();
-    const year = now.getFullYear();
-    const yearStart = new Date(year, 0, 1);
-    const yearEnd = new Date(year, 11, 31);
+    const currentMonth = now.getUTCMonth();
+    const currentYear = now.getUTCFullYear();
 
-    return date >= yearStart && date <= yearEnd;
+    const fyStartYear = currentMonth >= 3 ? currentYear : currentYear - 1;
+    const fyEndYear = fyStartYear + 1;
+
+    const fyStart = new Date(fyStartYear, 3, 1);
+    const fyEnd = new Date(fyEndYear, 2, 31);
+
+    return date >= fyStart && date <= fyEnd;
 };
 
 export const createExpenseSchema = z.object({
@@ -17,6 +22,6 @@ export const createExpenseSchema = z.object({
     amount: z.number("Amount must be a number").min(1, "Amount is required").positive("Amount must be greater than zero"),
     category: z.string("Category must be a string").trim().min(1, "Category is required").toUpperCase(),
     paymentMethod: z.refine((val) => paymentMethod.includes(val.toUpperCase()), { message: `Invalid payment method. Must be ${paymentMethod.join(", ")}` }),
-    transactionDate: z.coerce.date("Transaction date must be a valid date").refine(isWithinYear, "Transaction date must be within the current year"),
+    transactionDate: z.coerce.date("Transaction date must be a valid date").refine(isWithinYear, "Transaction date must be within the current financial year"),
     userId: z.number("User ID must be a number").int("User ID must be a whole number").positive("User ID must be a positive number")
 });
